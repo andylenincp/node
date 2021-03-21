0edb943e4f497a7821bee70b7692cb1c200a3e8c
Andy Castillo <andy.castillopalma@outlook.com>
2021-03-20 23:07:19 -0500

    Leer parámetros

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,9 +1,13 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
 
-app.use("/static", express.static('public'));
-app.use(express.static('assets'));
+app.use("/public", express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.set("view engine", "jade");
 
@@ -14,5 +18,11 @@ app.get("/", function(req,res) {
 app.get("/login", function(req,res) {
     res.render("login");
 });
+
+app.post("/users", function(req,res) {
+    console.log("Contraseña: "+ req.body.password);
+    console.log("Email: "+ req.body.email);
+    res.send("Datos recibidos");
+})
 
 app.listen(8080);

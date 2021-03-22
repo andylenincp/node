c7fd76674583fc6f664ea878d6ffc3c2150b9989
Andy Castillo <andy.castillopalma@outlook.com>
2021-03-22 15:23:05 -0500

    mongoose.connect

--- proyecto/app.js
+++ proyecto/app.js
@@ -3,6 +3,20 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost/fotos", { useUnifiedTopology: true, useNewUrlParser: true });
+
+var userSchemaJSON = {
+    email:String,
+    password:String
+};
+
+var user_schema = new Schema(userSchemaJSON);
+
+var User = mongoose.model("User", user_schema);
+
 app.use("/public", express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -16,13 +30,18 @@ app.get("/", function(req,res) {
 });
 
 app.get("/login", function(req,res) {
-    res.render("login");
+    User.find(function(err,doc) {
+        console.log(doc);
+        res.render("login");
+    });
 });
 
 app.post("/users", function(req,res) {
-    console.log("Contraseña: "+ req.body.password);
-    console.log("Email: "+ req.body.email);
-    res.send("Datos recibidos");
+    var user = new User({email: req.body.email, password: req.body.password});
+
+    user.save(function() {
+        res.send("Datos almacenados");
+    });
 })
 
 app.listen(8080);

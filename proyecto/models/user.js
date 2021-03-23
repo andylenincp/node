a6d073ae930b0c39f44a309d26b7c9b0e068d35b
Andy Castillo <andy.castillopalma@outlook.com>
2021-03-23 11:57:44 -0500

    Virtual password confirmation

--- proyecto/models/user.js
+++ proyecto/models/user.js
@@ -12,6 +12,12 @@ var user_schema = new Schema({
     date_of_birth: Date
 });
 
+user_schema.virtual("password_confirmation").get(function() {
+    return this.password_c;
+}).set(function(password) {
+    this.password_c = password;
+});
+
 var User = mongoose.model("User", user_schema);
 
 module.exports.User = User;

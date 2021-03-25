e718f9bbaf05136d37f6e272513e268d459a7dc9
Andy Castillo <andy.castillopalma@outlook.com>
2021-03-25 12:04:52 -0500

    mongoose find

--- proyecto/app.js
+++ proyecto/app.js
@@ -15,11 +15,15 @@ app.get("/", function(req,res) {
     res.render("index");
 });
 
-app.get("/login", function(req,res) {
+app.get("/signup", function(req,res) {
     User.find(function(err,doc) {
         console.log(doc);
-        res.render("login");
+        res.render("signup");
     });
+});
+
+app.get("/login", function(req,res) {
+    res.render("login");
 });
 
 app.post("/users", function(req,res) {
@@ -48,6 +52,13 @@ app.post("/users", function(req,res) {
         }
     });
     
+});
+
+app.post("/sessions", function(req,res) {
+    User.findOne({email:req.body.email,password:req.body.password}, function(err,docs){
+        console.log(docs);
+        res.send("Acceso correcto");
+    });
 });
 
 app.listen(8080);

23307e1c0addd2106eeb108c57c62854fbbbeb7f
Andy Castillo <andy.castillopalma@outlook.com>
2021-03-26 18:25:35 -0500

    Sesiones

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var User = require("./models/user").User;
+var session = require("express-session");
+
 var app = express();
 
 app.use("/public", express.static('public'));
@@ -8,10 +10,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(session({
+    secret: "andy12345alcp",
+    resave: false,
+    saveUninitialized: false
+}));
 
 app.set("view engine", "jade");
 
 app.get("/", function(req,res) {
+    console.log(req.session.user_id);
     res.render("index");
 });
 
@@ -55,9 +63,9 @@ app.post("/users", function(req,res) {
 });
 
 app.post("/sessions", function(req,res) {
-    User.findOne({email:req.body.email,password:req.body.password}, function(err,docs){
-        console.log(docs);
-        res.send("Acceso correcto");
+    User.findOne({email:req.body.email,password:req.body.password}, function(err,user){
+        req.session.user_id = user._id;
+        res.send("Funcionando");
     });
 });
 

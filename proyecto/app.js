81aa416f3a0e5e9ca422d9f90aea560b3b579b87
Andy Castillo <andy.castillopalma@outlook.com>
2021-03-27 11:42:42 -0500

    middleware

--- proyecto/app.js
+++ proyecto/app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var User = require("./models/user").User;
 var session = require("express-session");
+var router_app = require("./routes_app");
+var session_middleware = require("./middlewares/session");
 
 var app = express();
 
@@ -68,5 +70,8 @@ app.post("/sessions", function(req,res) {
         res.send("Funcionando");
     });
 });
+
+app.use("/app", session_middleware);
+app.use("/app", router_app);
 
 app.listen(8080);

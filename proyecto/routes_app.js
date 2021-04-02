bea40f927abf49b15225517e5289ab6b585b1bd6
Andy Castillo <andy.castillopalma@outlook.com>
2021-04-02 16:28:43 -0500

    Rutas de CRUD imágenes

--- proyecto/routes_app.js
+++ proyecto/routes_app.js
@@ -1,3 +1,4 @@
+const { Router } = require("express");
 var express = require("express");
 
 var router = express.Router();
@@ -5,5 +6,33 @@ var router = express.Router();
 router.get("/", function(req,res) {
     res.render("app/home");
 });
+
+router.get("/images/new", function(req,res) {
+    res.render("app/images/new");
+});
+
+router.get("/images/:id/edit", function(req,res) {
+    
+});
+
+/* REST */
+router.route("/images/:id")
+    .get(function(req,res) {
+        
+    })
+    .put(function(req,res) {
+        
+    })
+    .delete(function(req,res) {
+        
+    })
+
+router.route("/images")
+    .get(function(req,res) {
+        
+    })
+    .post(function(req,res) {
+        
+    })
 
 module.exports = router;

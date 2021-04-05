23723e0f5b2a31ba461c5f5315cc64c47c689e5a
Andy Castillo <andy.castillopalma@outlook.com>
2021-04-04 23:10:22 -0500

    Images model

--- proyecto/routes_app.js
+++ proyecto/routes_app.js
@@ -1,38 +1,50 @@
 const { Router } = require("express");
 var express = require("express");
-
+var Imagen = require("./models/imagenes");
 var router = express.Router();
 
 router.get("/", function(req,res) {
     res.render("app/home");
 });
 
-router.get("/images/new", function(req,res) {
-    res.render("app/images/new");
+router.get("/imagenes/new", function(req,res) {
+    res.render("app/imagenes/new");
 });
 
-router.get("/images/:id/edit", function(req,res) {
+router.get("/imagenes/:id/edit", function(req,res) {
     
 });
 
 /* REST */
-router.route("/images/:id")
+router.route("/imagenes/:id")
     .get(function(req,res) {
-        
+        Imagen.findById(req.params.id, function(err,imagen) {
+            res.render("app/imagenes/show", {imagen: imagen});
+        })
     })
     .put(function(req,res) {
         
     })
     .delete(function(req,res) {
         
-    })
+    });
 
-router.route("/images")
+router.route("/imagenes")
     .get(function(req,res) {
         
     })
     .post(function(req,res) {
-        
-    })
+        var data = {
+            title: req.body.title
+        }
+        var imagen = new Imagen(data);
+        imagen.save(function(err) {
+            if (!err) {
+                res.redirect("/app/imagenes/"+imagen._id);
+            } else {
+                res.render(err);
+            }
+        });
+    });
 
 module.exports = router;
